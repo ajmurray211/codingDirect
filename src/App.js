eef91b3c83800ab87f0e1d73e25ce8af97f632de
AJ Murray <aj.murr4y@gmail.com>
2024-07-14 02:22:03 -0700

    added in routes that navigate to different pages throughout the websiite possible bug with deployment after implimenting the has router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { Route, Routes } from 'react-router';
+import Navbar from './Components/Navbar.js'
+import Footer from './Components/Footer.js'
+import Home from './Pages/Home.js'
+import Work from './Pages/Work.js'
+import Pricing from './Pages/Pricing.js'
+import Contact from './Pages/Contact.js'
+import About from './Pages/About.js'
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Coding Direct</h1>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/work' element={<Work />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/About' element={<About />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return ( 
-        <h2>NavBar</h2>
-     );
+    return (
+        <>
+            <h2>NavBar</h2>
+            <Link to="/">Home</Link>
+            <Link to="/work">Work</Link>
+            <Link to="/pricing">Pricing</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/about">About</Link>
+        </>
+    );
 }
- 
+
 export default Navbar;
