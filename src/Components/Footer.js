f1754d72717c7a259c6fe3c45e570552a714cee7
AJ Murray <aj.murr4y@gmail.com>
2024-11-16 11:27:41 -0800

    rough creation of footer and nav. still need more styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,23 @@
 import logo from '../Assets/logo-2.png'
+import icon1 from '../Assets/instagramIcon.png'
+import icon2 from '../Assets/tiktokIcon.png'
+import icon3 from '../Assets/facebookIcon.png'
 
 const Footer = () => {
     return (
         <div id='footer'>
             <img className='CDlogo' src={logo} />
-            <div> ©2024. All Rights Reserved.</div>
-            <div>
-                <img/>
-                <img/>
-                <img/>
+            <div id='footerCopyright'> ©2024. All Rights Reserved.</div>
+            <div id='footerLinksContainer'>
+                <a target='_blank' href=''>
+                    <img className='footerIcon' src={icon1} />
+                </a>
+                <a target='_blank' href=''>
+                    <img className='footerIcon' src={icon2} />
+                </a>
+                <a target='_blank' href=''>
+                    <img className='footerIcon' src={icon3} />
+                </a>
             </div>
         </div>
     );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from '../Assets/logo-2.png'
 
 const Navbar = () => {
+    const location = useLocation()
+
     return (
         <div id="navContainer">
             <img className="CDlogo" src={logo} />
-            <Link className="navLink" to="/">Home</Link>
-            <Link className="navLink" to="/projects">Projects</Link>
-            {/* <Link className="navLink" to="/pricing">Pricing</Link> */}
-            <Link className="navLink" to="/contact">Contact</Link>
-            {/* <Link className="navLink" to="/about">About</Link> */}
+            <section id="navLinks">
+                <Link className={`navLink ${location.pathname === "/" ? "active" : ""}`} to="/">Home</Link>
+                <Link className={`navLink ${location.pathname === "/projects" ? "active" : ""}`} to="/projects">Projects</Link>
+                <Link className={`navLink ${location.pathname === "/contact" ? "active" : ""}`} to="/contact">Contact</Link>
+
+            </section>
         </div >
     );
 }
