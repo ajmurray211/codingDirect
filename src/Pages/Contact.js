f586f430736604e0eaa84b1c224cd04bec6c73c2
AJ Murray <aj.murr4y@gmail.com>
2024-11-18 09:14:07 -0800

    finished styling the contact page. fixed the resizing issue on theninputs

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,6 +1,9 @@
 import { Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
 import emailjs from 'emailjs-com';
 import { useState } from "react";
+import icon1 from '../Assets/instagramIcon.png'
+import icon2 from '../Assets/tiktokIcon.png'
+import icon3 from '../Assets/facebookIcon.png'
 
 const Contact = () => {
     const [emailData, setEmailData] = useState()
@@ -32,70 +35,63 @@ const Contact = () => {
         <div id="contact">
             <div id="contactForm">
                 <h2 id="contactTitle">GET IN TOUCH</h2>
-                <div>
-                    <Form id="contactInputs">
-                        {/* First Row: First Name and Last Name */}
-                        <Row form className="d-flex justify-content-between">
-                            <Col md={6} className="d-flex" style={{ maxWidth: '48%' }}>
-                                <FormGroup style={{ flex: 1 }}>
-                                    <Input placeholder="First name *" />
-                                </FormGroup>
-                            </Col>
-                            <Col md={6} className="d-flex" style={{ maxWidth: '48%' }}>
-                                <FormGroup style={{ flex: 1 }}>
-                                    <Input placeholder="Last name *" />
-                                </FormGroup>
-                            </Col>
-                        </Row>
+                <Form id="contactInputs">
+                    <Row>
+                        <Col sm={6}>
+                            <FormGroup>
+                                <Input placeholder="First name *" />
+                            </FormGroup>
+                        </Col>
+                        <Col sm={6}>
+                            <FormGroup>
+                                <Input placeholder="Last name *" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={6}>
+                            <FormGroup>
+                                <Input
+                                    invalid={formErrors.email}
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    placeholder="Email *"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <span className="text-danger">{formErrors.email}</span>
+                            </FormGroup>
+                        </Col>
+                        <Col sm={6}>
+                            <FormGroup>
+                                <Input placeholder="Phone number *" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
 
-                        {/* Second Row: Email and Phone Number */}
-                        <Row form className="d-flex justify-content-between">
-                            <Col md={6} className="d-flex" style={{ maxWidth: '48%' }}>
-                                <FormGroup style={{ flex: 1 }}>
-                                    <Input
-                                        invalid={formErrors.email}
-                                        type="email"
-                                        name="email"
-                                        id="email"
-                                        placeholder="Email *"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                    <span className="text-danger">{formErrors.email}</span>
-                                </FormGroup>
-                            </Col>
-                            <Col md={6} className="d-flex" style={{ maxWidth: '48%' }}>
-                                <FormGroup style={{ flex: 1 }}>
-                                    <Input placeholder="Phone number *" />
-                                </FormGroup>
-                            </Col>
-                        </Row>
-
-                        {/* Third Row: Full Width Message Textarea */}
-                        <Row form>
-                            <Col md={12}>
-                                <FormGroup>
-                                    <Input type="textarea" placeholder="Message *" />
-                                </FormGroup>
-                            </Col>
-                        </Row>
-
-                        <Button id="ContactSend">Inquire Now</Button>
-                    </Form>
-                </div>
+                    <Row>
+                        <Col sm={12}>
+                            <FormGroup>
+                                <Input type="textarea" placeholder="Message *" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                </Form>
+                <Button id="ContactSend">Inquire Now</Button>
             </div>
 
             <div id="contactFollow">
                 <p id="contacttitle2">FOLLOW US</p>
                 <div id="contactIcons">
                     <div className="contactIconContainer">
-                        <img className="contactIcon" alt="social-icon-1" />
+                        <img src={icon1} className="contactIcon" alt="social-icon-1" />
                     </div>
                     <div className="contactIconContainer">
-                        <img className="contactIcon" alt="social-icon-2" />
+                        <img src={icon2} className="contactIcon" alt="social-icon-2" />
                     </div>
                     <div className="contactIconContainer">
-                        <img className="contactIcon" alt="social-icon-3" />
+                        <img src={icon3} className="contactIcon" alt="social-icon-3" />
                     </div>
                 </div>
             </div>
